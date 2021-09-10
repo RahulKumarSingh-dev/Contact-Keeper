88c08c9b2b16c296126510aa8c2f789437f1ef31
Rahul Kumar <rahulkumar020702@gmail.com>
2021-09-10 18:38:43 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDb = require('./config/db');
 const app = express();
-app.get('/', (req, res) => res.json({msg:"Welcome to contact keeper API"}));
+
+//Connect db
+connectDb();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.json({ msg: 'Welcome to contact keeper API' }));
 //Define routes
-app.use('/api/users',require('./routes/users'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/contacts',require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on prot ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
